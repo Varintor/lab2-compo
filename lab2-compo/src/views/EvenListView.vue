32366eaede3942b2c98f783ffd3d7d82fc542936
Varintor <varintorn_si@cmu.ac.th>
2025-07-13 19:09:04 +0700

    finish

---
+++ lab2-compo/src/views/EvenListView.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue'
+import CategoryInfo from '@/components/CategoryInfo.vue'
+import type { Event } from '@/type'
+import { ref, onMounted } from 'vue'
+import EventService from '@/services/EventService'
+
+const events = ref<Event[]>([])
+
+onMounted(() => {
+  EventService.getEvents()
+    .then((response) => {
+      console.log(response.data)
+      events.value = response.data.events
+    })
+    .catch((error) => {
+      console.log('There was an error!', error)
+    })
+})
+
+</script>
+
+<template>
+  <h1>Events For Good</h1>
+  <!--new element-->
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+
+  <div class="category">
+    <CategoryInfo v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.category {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+}
+</style>
