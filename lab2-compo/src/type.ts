02b3ada95c7c6ee531c69c53767dfee5feebb9d0
Varintor <varintorn_si@cmu.ac.th>
2025-07-13 18:00:29 +0700

    9.3 new elements

---
+++ lab2-compo/src/type.ts
@@ -0,0 +1,11 @@
+export interface Event {
+    id: number
+    category:String
+    title:String
+    description: String
+    location: String
+    date: String
+    time: String
+    petsAllowed: boolean
+    organizer: String
+}
--- lab2-compo/src/views/HomeView.vue
+++ lab2-compo/src/views/HomeView.vue
@@ -1,5 +1,66 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue'
+import CategoryInfo from '@/components/CategoryInfo.vue'
+import type { Event } from '@/type'
+import { ref } from 'vue'
+
+const events = ref<Event[]>([
+  {
+    id: 5928101,
+  category: 'animal welfare',
+  title: 'Cat Adoption Day',
+  description: 'Find your new feline friend at this event.',
+  location: 'Meow Town',
+  date: 'January 28, 2022',
+  time: '12:00',
+  petsAllowed: true,
+  organizer: 'Kat Laydee'
+  },
+  {
+    id:4582797,
+    category: 'food',
+    title: 'Community Gardening',
+    description: 'Join us as we tend to the community edible plants.',
+    location: 'Flora City',
+    date: 'March 14,2022',
+    time: '10:00',
+    petsAllowed: true,
+    organizer:'Fern Pollin'
+  },{
+    id:8419988,
+    category: 'sustainbility',
+    title: 'Beach Cleanup',
+    description: 'Help pick up trash along the shore.',
+    location: 'Playa Del Carmen',
+    date: 'July 22,2022',
+    time: '11:00',
+    petsAllowed: false,
+    organizer:'Carey Wales'
+  }
+])
+</script>
 
 <template>
-  <div class="home"></div>
+  <h1>Events For Good</h1>
+  <!--new element-->
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+
+  <div class="category">
+    <CategoryInfo v-for="event in events" :key="event.id" :event="event" />
+  </div>
 </template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.category {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+}
+</style>
